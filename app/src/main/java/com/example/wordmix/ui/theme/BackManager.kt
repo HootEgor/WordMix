a35195f898ecce79d1bb0291d678fdcf724cb1a3
HootEgor <egor.hoot@gmail.com>
2023-02-15 15:35:48 +0100

    Simple FireBase GET

--- app/src/main/java/com/example/wordmix/ui/theme/BackManager.kt
+++ app/src/main/java/com/example/wordmix/ui/theme/BackManager.kt
@@ -1,7 +1,22 @@
 package com.example.wordmix.ui.theme
 
-class BackManager {
-    fun test(){
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
+class BackManager {
+    val db = Firebase.firestore
+
+    fun test(){
+        db.collection("Users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("EEE", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("EEE", "Error getting documents.", exception)
+            }
     }
 }
--- app/src/main/java/com/example/wordmix/ui/theme/User.kt
+++ app/src/main/java/com/example/wordmix/ui/theme/User.kt
@@ -3,7 +3,9 @@ package com.example.wordmix.ui.theme
 import java.net.PasswordAuthentication
 
 data class User(
+    val ID: String,
     val login: String,
-    val password: String
+    val password: String,
+    val language: Int
 ){
 }
